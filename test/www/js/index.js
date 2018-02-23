5c1b6832635b49c199b6b4804a8140af52a6ed12
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-02-23 16:28:05 +0900

    カメラ動かすところまでやった

--- test/www/js/index.js
+++ test/www/js/index.js
@@ -1,11 +1,32 @@
 
 // for cordova
 document.addEventListener('deviceready', function() {
-  document.getElementById("button").onclick = function() {
-    if (window.cordova) {
-      scanner.scan(function(data) {
-        window.alert(JSON.stringify(data));
-      }, function() {}, 'foooo');
-    }
+  if (!window.cordova) {
+    return;
+  }
+  document.getElementById("start").onclick = function() {
+
+    scanner.start(function(data) {
+      // window.alert('');
+    }, function() {
+      alert('失敗');
+    });
+  
+  };
+  document.getElementById("scan").onclick = function() {
+    scanner.scan(function(data) {
+      var elm = document.createElement('pre');
+      elm.textContent = data;
+      document.body.appendChild(elm);
+    }, function() {
+      alert('失敗');
+    }, '');
+  };
+  document.getElementById("stop").onclick = function() {
+    scanner.stop(function(data) {
+      // window.alert('');
+    }, function() {
+      alert('失敗');
+    });
   };
 });
